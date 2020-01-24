c13210c946bf826a68a3f57728eefe18c30dbc8e
allison_johnson <allison.cuttler@gmail.com>
2020-01-23 17:18:16 -0800

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,7 +19,7 @@ class InteractiveRecord
     end #table_name_for_insert
 
     def self.column_names
-      DB[:conn].results_as_hash = true
+      #DB[:conn].results_as_hash = true
 
       sql = "pragma table_info('#{table_name}')"
   
@@ -29,10 +29,11 @@ class InteractiveRecord
       column_names = []
       table_info.each do |row|
         #Picks of the value associated with the "name" key for each row hash
-        column_names << row["name"]
+        column_names << row[1]
       end
       #Removes any 'nil' values from the array before returning it
-      column_names_without_indexes = column_names.compact.delete_if{|name| name.is_a? Integer}
+      column_names.compact
+      #column_names_without_indexes = column_names.compact.delete_if{|name| name.is_a? Integer}
       #binding.pry
     end #self.column_names
 
@@ -52,6 +53,30 @@ class InteractiveRecord
     def save
       sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
       DB[:conn].execute(sql)
+      #DB[:conn].execute("SELECT * FROM #{table_name_for_insert} WHERE id = (SELECT MAX(id) FROM #{table_name_for_insert})")[0][0]
+      id_info = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")
+      @id = id_info[0][0]
+      #binding.pry 
     end #save
-  
+
+    def self.find_by_name(a_name)
+      sql = <<-SQL
+        SELECT * FROM #{self.table_name}
+        WHERE name = ?
+        LIMIT 1
+      SQL
+      DB[:conn].execute(sql, a_name)
+    end #self.find_by_name
+
+    def self.find_by(attr_hash)
+      key = attr_hash.keys[0]
+      val = attr_hash[attr_hash.keys[0]]
+      sql = <<-SQL
+        SELECT * FROM #{self.table_name}
+        WHERE #{key} = '#{val}'
+      SQL
+      #binding.pry 
+      DB[:conn].execute(sql)
+    end #self.find_by
+    
 end #class
