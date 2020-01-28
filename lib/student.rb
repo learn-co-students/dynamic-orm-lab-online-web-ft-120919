bb36044b7269b0771e37c78276a1a83d7021ee8a
ricardo39985 <ricardo39985@gmail.com>
2020-01-27 19:55:17 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,14 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-    df self.table_name
-        sql = <<-SQL
-        CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        grade INTEGER
-        )
-        SQL
-        DB[:conn].execute(sql)
-    end
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+      end
 end
