827eb45fe3a5fe564a5422d78eabda92d27ad85f
Uchoosecode <awittyl44@gmail.com>
2020-01-31 18:25:59 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,34 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+
+    def self.column_names
+        DB[:conn].results_as_hash = true
+
+        sql = "PRAGMA table_info('#{table_name}')"
+
+        table_info = DB[:conn].execute(sql)
+            column_names = []
+
+            table_info.each do |col|
+                column_names << col["name"]
+            end
+            column_names.compact
+    end
+
+    def initialize(attrs = {})
+        attrs.each do |key, value|
+            self.send("#{key}=", value) # if self.respond_to?("#{key}")
+        binding.pry
+        end
+        
+    end
 
 end
