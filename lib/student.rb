37b88ed5bd0fb800f8ed2827f69d48df6056203d
Uchoosecode <awittyl44@gmail.com>
2020-01-31 19:23:34 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,31 +5,10 @@ require 'pry'
 
 class Student < InteractiveRecord
    
-    def self.table_name
-        self.to_s.downcase.pluralize
-    end
-
-    def self.column_names
-        DB[:conn].results_as_hash = true
-
-        sql = "PRAGMA table_info('#{table_name}')"
-
-        table_info = DB[:conn].execute(sql)
-            column_names = []
-
-            table_info.each do |col|
-                column_names << col["name"]
-            end
-            column_names.compact
-    end
-   
-    def initialize(attrs={})
-        attrs.each do |key, value|
-            # binding.pry 
-            self.send("#{key}=", value)
-        end
-        
-    end
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+      end
+  
     
 
 end
