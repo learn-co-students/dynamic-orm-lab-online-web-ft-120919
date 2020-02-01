529b444d59d4d9b1357fcc5447fc6c24f39bff0e
Uchoosecode <awittyl44@gmail.com>
2020-02-01 15:09:46 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,9 +26,30 @@ class InteractiveRecord
             self.send("#{key}=", value) if self.respond_to?("#{key}=")
         end   
     end
-    # binding.pry 
+    
     def table_name_for_insert
-        
+        self.class.table_name
     end
     
+    def col_names_for_insert
+        self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+    end
+    
+    def values_for_insert
+        values = []
+        self.class.column_names.each do |col_name|
+            values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end
+        values.join(", ")
+    end
+    
+    def save
+        sql = "INSERT INTO #{table_name_for_insert} #{col_names_for_insert} VALUES (?)", "[#{values_for_insert}]"
+        DB[:conn].execute(sql)
+        binding.pry
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+    
+
+
 end
