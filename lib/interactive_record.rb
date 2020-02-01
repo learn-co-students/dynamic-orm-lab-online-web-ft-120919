8766746bba612d33133dd3a4da5a7623bbf69757
Uchoosecode <awittyl44@gmail.com>
2020-02-01 15:28:08 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,6 +48,7 @@ class InteractiveRecord
         DB[:conn].execute(sql)
         binding.pry
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    
     end
     
 
